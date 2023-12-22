d2f40fcdf14e55f66861b7bc2c55d77d64f817b8
AlexandraGDev <alexandra.gonzalez.dev@gmail.com>
2023-12-22 17:40:03 -0500

    se implementa mejora en el responsive

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 import "./Nav.css";
 import logoWebsite from "../../assets/logo/logoWebsite2.png";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <header className="header">
-      <img src={logoWebsite} alt="Game website logo" className="logoWebsite"/>
+      <img src={logoWebsite} alt="Game website logo" className="logoWebsite" />
 
       <nav className="navBar">
         <ul>
           <li>
-            <a href="/" className="navBarLinks">
+            <Link to="/" className="navBarLinks">
               Home
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="/games" className="navBarLinks">
+            <Link to="/games" className="navBarLinks">
               Games
-            </a>
+            </Link>
           </li>
-          <li>
-            <a href="/" className="navBarLinks">
+          {/* <li>
+            <Link to="/" className="navBarLinks">
               About
-            </a>
-          </li>
+            </Link>
+          </li> */}
         </ul>
       </nav>
     </header>
